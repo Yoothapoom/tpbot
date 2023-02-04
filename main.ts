db6d8be67ecc8e79302aebd2fbeb54dfd84c8f77
Yoothapoom <124431528+Yoothapoom@users.noreply.github.com>
2023-02-04 17:17:19 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 basic.showString("Hello!")
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-	
+    TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 50)
+    if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 5)) {
+        TPBot.setTravelTime(TPBot.DriveDirection.Backward, 50, 3)
+        TPBot.setTravelTime(TPBot.DriveDirection.Left, 50, 1)
+    }
 })
